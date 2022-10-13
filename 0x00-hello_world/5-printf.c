8e4a537232353050c8fbaf6a70cd357d067cd23a
Davidile <mugomadavid24@gmail.com>
2022-10-13 13:10:21 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Print a text 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a peace of art, \n");
+     return (0);
+
+ }
