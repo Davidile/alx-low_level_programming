e6a14515b41118a83fea957e95277f23eff574c7
Davidile <mugomadavid24@gmail.com>
2022-10-14 15:39:59 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Description: Printing digitt number from 0 to 10
+ * Return 0.
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 10 ; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
