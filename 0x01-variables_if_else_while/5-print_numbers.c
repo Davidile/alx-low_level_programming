c69a3f03206537e12d5dcd82b44720dd8268a88b
Davidile <mugomadavid24@gmail.com>
2022-10-14 15:44:21 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,12 +2,12 @@
 /**
  * main - main function
  * Description: Printing digitt number from 0 to 10
- * Return 0.
+ * Return: 0.
  */
 int main(void)
 {
 int i;
-for (i = 0; i <= 10 ; i++)
+for (i = 0; i <= 9 ; i++)
 {
 printf("%d", i);
 }
