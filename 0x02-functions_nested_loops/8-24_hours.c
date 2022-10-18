f991c7a8d146a19d5e21a23c072c69c23ed8ccb5
Davidile <mugomadavid24@gmail.com>
2022-10-18 13:49:25 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,8 +14,8 @@ for (minute = 0; minute <= 59 ; minute++)
 _putchar('0' + (hour / 10));
 _putchar('0' + (hour % 10));
 _putchar(':');
-_putchar('0' + (minute / 10);
-_putchar('0' + (minute % 10);
+_putchar('0' + (minute / 10));
+_putchar('0' + (minute % 10));
 _putchar('\n');
 }
 }
