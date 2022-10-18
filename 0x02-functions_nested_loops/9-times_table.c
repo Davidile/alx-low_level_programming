ac994aaa1c9d312ff394e6c3368e97083d5ad12b
Davidile <mugomadavid24@gmail.com>
2022-10-18 14:22:26 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,40 +1,26 @@
 #include "main.h"
 
 /**
- *
- *  *times_table - 9 times table
- *
- *   *
- *
- *    *Return: no return value
- *
- *     *
- *
- *      */
+ *times_table - 9 times table
+ *Return: no return value
+ */
 
 void times_table(void)
 
 {
 
-	int i, j, product;
+int i, j, product;
 
-	for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 
-	{
-
-		for (j = 0; j < 10;  j++)
-
-		{
-
-			product = i * j;
-
-			if (j == 0)
-
-			{
-
-				_putchar('0' + product);
-
-			}
+{
+for (j = 0; j < 10;  j++)
+{
+product = i * j;
+if (j == 0)
+{
+_putchar('0' + product);
+}
 
 			else if (product >= 10)
 
